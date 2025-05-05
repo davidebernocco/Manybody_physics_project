bb183086bc0e53a7f4ffdd753256640c04710897
Davide Bernocco <davide.plantageneto@gmail.com>
2025-05-05 16:33:28 +0200

    Add files via upload

---
+++ Funz_ladder.py
@@ -0,0 +1,6 @@
+"""
+Self built functions for the study of the Heisemberg S=1/2 model on a ladder
+
+@author: david
+"""
+
---
+++ Ladder.py
@@ -0,0 +1,8 @@
+"""
+Heisemberg S=1/2 model on a classic ladder.
+(PBC on the two parallel legs)
+
+@author: david
+"""
+
+
